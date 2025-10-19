2b3dbeab2b3c818c3b5f11e9ed14e9fe9e8d2968
FernandoH <fernando.huilca@epn.edu.ec>
2025-10-18 23:09:05 -0500

    Inicio

--- configuraridioma.php
+++
@@ -1,7 +0,0 @@
-<?php
-    if (isset($_GET['lang'])){
-        setcookie('lang', $_GET['lang'], time() + (86400 * 30), "/"); // 86400 = 1 day
-    } 
-    header("Location: panelprincipal.php");
-
-?>
--- panelprincipal.php
+++ panelprincipal.php
@@ -1,40 +1,22 @@
-<?php
-session_start();
-$lang = isset($_COOKIE['lang']) ?  $_COOKIE['lang']  : 'es';
-$title = $lang == 'es' ? 'Lista de Productos' : 'Product List';  
-$path = 'Recursos/categorias_' . $lang . '.txt';
-$_productos = file($path);
-?>
 
 
+<!DOCTYPE html>
 <html>
-    <head></head> 
+    <head>
+        <meta charset="UTF-8">
+        <title>PanelPrincipal</title>
+    </head>
+
     <body>
-        <h1>PANEL PRINCIPAL</h1>
-         <h2>Bienvenido Usuario: <?php echo $_SESSION['usuario']  ?></h2> 
-        <!--<h2>Bienvenido Usuario: nombre_de_usuario </h2>-->
-        <br>
-        <a href="configuraridioma.php?lang=es">ES (Español) </a>
-         | 
-        <a href="configuraridioma.php?lang=en">EN (English) </a>
-        <br>
-        <br>
-        <a href="cerrarsesion.php"> Cerrar Sesion </a>
+        <h1> PANEL PRINCIPAL </h1>
+        <h3> Bienvenido usuario: Fernando </h3>
+        <a href = "" >ES (Español) </a> |
+        <a href = "" >EN (English) </a>
+        <br> 
+        <a href = "cerrarsesion.php">Cerrar Sesion</a>
 
-        <h3><?php echo $title ?></h3>
-
-        <?php
-        foreach($_productos as $producto){
-            $datos = explode(",", $producto);
-            echo '<p><a href="producto.php?id='.intval($datos[0]).'">'.htmlspecialchars($datos[1]).'</a></p>';
+        
 
 
-            // Revisar orden de elementos id del producto, nombre, descripcion, precio
-            //if (count($datos) >= 2) {
-            //  $id = trim($datos[0]);       
-            //  $nombre = trim($datos[1]);   
-            //  echo '<p><a href="producto.php?id=' . intval($id) . '">' . htmlspecialchars($nombre) . '</a></p>';
-        }
-        ?>
     </body>
 </html>
